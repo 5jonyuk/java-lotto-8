399be8e63f5ee29d3d34157785ca2a1e149e8664
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-10-31 12:37:09 +0900

    feat(service): 구입 수량에 맞춰 개별 로또 생성 기능 추가
    - Randoms.pickUniqueNumbersInRange()를 사용해 1~45 사이의 중복 없는 6개의 숫자를 생성
    - createLotto() 메서드에서 단일 로또 객체 생성 로직 구현
    - generateLotto() 메서드에서 구입 수량(count)에 맞는 로또 리스트 생성

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,9 +1,31 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
 import lotto.domain.PurchaseCount;
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoService {
+    private static final int LOTTO_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     public PurchaseCount createPurchaseCount(int amount) {
         return new PurchaseCount(amount);
+    }
+    public List<Lotto> generateLotto(int count){
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = 0; i < count; i++){
+            lottos.add(createLotto());
+        }
+        return lottos;
+    }
+
+    private Lotto createLotto(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE);
+        return new Lotto(numbers);
     }
 }
