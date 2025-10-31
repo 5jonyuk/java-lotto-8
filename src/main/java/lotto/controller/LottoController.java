fb4776fdfe9629036ef2d053f6fab6c30c9ea361
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-10-31 14:34:30 +0900

    feat(controller): 전체 흐름 제어 구현
    - 로또번호 생성 -> 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,12 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
 import lotto.domain.PurchaseCount;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.List;
 
 public class LottoController {
     private final InputView inputView;
@@ -22,5 +25,9 @@ public class LottoController {
 
         PurchaseCount purchaseCount = lottoService.createPurchaseCount(amount);
         outputView.printEnterPurchaseCountMessage(purchaseCount.getCount());
+
+        List<Lotto> lottos = lottoService.generateLotto(purchaseCount.getCount());
+        outputView.printLottos(lottos);
+
     }
 }
