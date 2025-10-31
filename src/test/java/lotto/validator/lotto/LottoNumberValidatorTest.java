cd776c3c1d84cd2df69c67bb79dbca8c345d5867
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-10-31 14:59:01 +0900

    tset: 로또번호 검증로직 테스트 추가

---
+++ src/test/java/lotto/validator/lotto/LottoNumberValidatorTest.java
@@ -0,0 +1,26 @@
+package lotto.validator.lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoNumberValidatorTest {
+    @Test
+    void 로또번호가_6개가_초과되면_예외가_발생한다() {
+        List<Integer> lottos = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        assertThatThrownBy(() -> LottoNumberValidator.validate(lottos))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또번호가_중복되면_예외가_발생한다() {
+        List<Integer> lottos = List.of(1, 2, 3, 4, 5, 5);
+
+        assertThatThrownBy(() -> LottoNumberValidator.validate(lottos))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
