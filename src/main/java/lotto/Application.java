4a7f4365356e5fe35aadf5e4a8bbe9ac660e7ce3
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-10-31 14:36:05 +0900

    feat(Application): 객체 생성 및 주입 로직 추가
    - 로또 번호를 주어진 형식에 맞게 출력하기 위해 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,18 +2,20 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.parser.StringToIntParser;
-import lotto.validator.PurchaseAmountValidator;
+import lotto.validator.input.PurchaseAmountValidator;
 import lotto.view.OutputView;
 import lotto.view.InputView;
 import lotto.service.LottoService;
-import lotto.validator.InputValidator;
+import lotto.validator.input.InputValidator;
 import lotto.parser.InputParser;
+import lotto.util.FormatLottoNumbers;
 
 public class Application {
     public static void main(String[] args) {
         InputValidator inputValidator = new PurchaseAmountValidator();
         InputParser inputParser = new StringToIntParser();
-        OutputView outputView = new OutputView();
+        FormatLottoNumbers formatLottoNumbers = new FormatLottoNumbers();
+        OutputView outputView = new OutputView(formatLottoNumbers);
         InputView inputView = new InputView(inputValidator, outputView, inputParser);
         LottoService lottoService = new LottoService();
 
