03a5ebe851848829c2e0e4f7a3885370421ef265
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-10-31 14:31:45 +0900

    feat(view): 로또 번호 출력 기능 추가
    - util에서 포맷기능을 사용

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,18 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.util.FormatLottoNumbers;
+
+import java.util.List;
+
 public class OutputView {
-    private final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private final String ENTER_PURCHASE_COUNT_MESSAGE = "%n%d개를 구매했습니다.";
+    private static final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String ENTER_PURCHASE_COUNT_MESSAGE = "%n%d개를 구매했습니다.%n";
+    private final FormatLottoNumbers formatLottoNumbers;
+
+    public OutputView(FormatLottoNumbers formatLottoNumbers) {
+        this.formatLottoNumbers = formatLottoNumbers;
+    }
 
     public void printEnterPurchaseAmountMessage() {
         System.out.println(ENTER_PURCHASE_AMOUNT_MESSAGE);
@@ -14,5 +24,12 @@ public class OutputView {
 
     public void printErrorMessage(String message) {
         System.out.println(message);
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        lottos.forEach(lotto -> {
+            String numbers = formatLottoNumbers.format(lotto.getNumbers());
+            System.out.println(numbers);
+        });
     }
 }
