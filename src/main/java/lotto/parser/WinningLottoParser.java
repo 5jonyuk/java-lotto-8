b66e759656b4020054c5d98bdadf51ce0e7186c5
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-11-02 02:20:25 +0900

    fix(parser): 1,2,3,4,5,6,,,와 같은 입력 예외처리 되도록 수정
    테스트를 진행하다 발견하여 수정함

--- src/main/java/lotto/parser/WinningLottoParser.java
+++ src/main/java/lotto/parser/WinningLottoParser.java
@@ -7,7 +7,7 @@ public class WinningLottoParser {
     private static final String LOTTO_NUMBER_SEPARATOR = ",";
 
     public List<Integer> parseWinningNumbers(String input) {
-        return Arrays.stream(input.split(LOTTO_NUMBER_SEPARATOR))
+        return Arrays.stream(input.split(LOTTO_NUMBER_SEPARATOR,-1))
                 .map(Integer::parseInt)
                 .toList();
     }
